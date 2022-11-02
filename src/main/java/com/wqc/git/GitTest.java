6b843faee608d53c8c4a6c5c2d04315220790432
2631735945 <86043687+2631735945@users.noreply.github.com>
2022-11-02 21:20:43 +0800

    Update GitTest.java

--- src/main/java/com/wqc/git/GitTest.java
+++ src/main/java/com/wqc/git/GitTest.java
@@ -13,5 +13,7 @@ public class GitTest {
         System.out.println("hello world");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
+        
     }
 }
